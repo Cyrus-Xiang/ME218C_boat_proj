acd9cf6c22c2bc5fa58a8a2b20ae245eda02d097
Cyrus Xiang <126836571+Cyrus-Xiang@users.noreply.github.com>
2025-05-19 22:06:28 -0700

    button press now modify txFrame

--- SPECTACOAL_PIC0/ProjectSource/controllerFSM.c
+++ SPECTACOAL_PIC0/ProjectSource/controllerFSM.c
@@ -268,10 +268,12 @@ ES_Event_t RuncontrollerFSM(ES_Event_t ThisEvent)
     if (ThisEvent.EventType == ES_DROP_COAL_BUTTON_PRESSED)
     {
       DB_printf("Drop coal event received\n");
+      txFrame[buttons_byte] &= 0b01; // set the drop coal bit
     }
     else if (ThisEvent.EventType == ES_DROP_ANCHOR_BUTTON_PRESSED)
     {
       DB_printf("Drop anchor event received\n");
+      txFrame[buttons_byte] &= 0b10; // set the drop anchor bit
     }
     else if (ThisEvent.EventType == ES_IMU_ORIENTATION_SWITCH && ThisEvent.EventParam == 1)
     {
